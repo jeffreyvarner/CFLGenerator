5e24db83a4e0c70f5a26da62fee64150d8709d38
jeffreyvarner <jeff@curveos.com>
2013-05-07 05:42:31 -0400

    Initial Commit

---
+++ CFLGenerator/VLDocument.h
@@ -0,0 +1,13 @@
+//
+//  VLDocument.h
+//  CFLGenerator
+//
+//  Created by Jeffrey Varner on 5/7/13.
+//  Copyright (c) 2013 Varnerlab. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface VLDocument : NSDocument
+
+@end
---
+++ CFLGenerator/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CFLGenerator
+//
+//  Created by Jeffrey Varner on 5/7/13.
+//  Copyright (c) 2013 Varnerlab. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
